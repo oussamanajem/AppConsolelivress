856addfb0566351dd1b0788b3a79195618ede999
oussama <300141420@monboreal.ca>
2024-12-07 12:51:53 -0500

    mais .gitignore

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppConsoleLivres
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenue dans l'application de gestion des livres!");
+        }
+    }
+}
+
